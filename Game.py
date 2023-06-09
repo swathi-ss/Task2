8f2bd9e6baee9691e796eb06de91642d118dcc9e
swathi-ss <132314374+swathi-ss@users.noreply.github.com>
2023-06-09 12:50:31 +1000

    9/6/23

--- Game.py
+++ Game.py
@@ -28,7 +28,7 @@ font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 game_over = font.render("Game Over", True, BLACK)
 
-background = pygame.image.load("AnimatedStreet.png")
+background = pygame.image.load("G:\My Drive\SDD\Task2\AnimatedStreet.png")
 
 #Create a white screen 
 DISPLAYSURF = pygame.display.set_mode((400,600))
@@ -39,7 +39,7 @@ pygame.display.set_caption("Game")
 class Enemy(pygame.sprite.Sprite):
       def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("Enemy.png")
+        self.image = pygame.image.load("G:\My Drive\SDD\Task2\Enemy.png")
         self.rect = self.image.get_rect()
         self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
 
@@ -55,7 +55,7 @@ class Enemy(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__() 
-        self.image = pygame.image.load("Player.png")
+        self.image = pygame.image.load("G:\My Drive\SDD\Task2\Player.png")
         self.rect = self.image.get_rect()
         self.rect.center = (160, 520)
        
@@ -109,7 +109,7 @@ while True:
 
     #To be run if collision occurs between Player and Enemy
     if pygame.sprite.spritecollideany(P1, enemies):
-          pygame.mixer.Sound('crash.wav').play()
+          pygame.mixer.Sound('G:\My Drive\SDD\Task2\crash.wav').play()
           time.sleep(1)
                    
           DISPLAYSURF.fill(RED)
